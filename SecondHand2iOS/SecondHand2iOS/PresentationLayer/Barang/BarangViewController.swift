5340ce03767da570afb5982503acb3257db231fe
Muh Akbar Idris <muhakbaridris@gmail.com>
2022-06-14 13:02:34 +0800

    Add feature/Barang and change ios support to 14.0

---
+++ SecondHand2iOS/SecondHand2iOS/PresentationLayer/Barang/BarangViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BarangViewController.swift
+//  SecondHand2iOS
+//
+//  Created by Muh Akbar Idris on 14/06/22.
+//
+
+import UIKit
+
+final class BarangViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.systemGray
+    }
+}
