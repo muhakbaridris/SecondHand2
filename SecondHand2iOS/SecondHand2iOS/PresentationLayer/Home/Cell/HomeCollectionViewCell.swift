622060c397a33f8a63cc79b282863d5e14233e85
nansyiiah <bosspetrik1403@gmail.com>
2022-06-16 18:43:29 +0700

    add feature home

---
+++ SecondHand2iOS/SecondHand2iOS/PresentationLayer/Home/Cell/HomeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCollectionViewCell.swift
+//  SecondHand2iOS
+//
+//  Created by Adrian K on 16/06/22.
+//
+
+import UIKit
+
+final class HomeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var nameCell: UILabel!
+    @IBOutlet var viewCell: UIView!
+    @IBOutlet weak var imageCell: UIImageView!
+}
