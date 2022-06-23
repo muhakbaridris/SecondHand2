2123fd8a20a6c8dd03610c9882213bcfb217d359
Maulana Frasha <ul.frasha@gmail.com>
2022-06-23 19:03:27 +0700

    fix authAPI *not finished

--- SecondHand2iOS/SecondHand2iOS/DataLayer/API/SHAuthAPI.swift
+++ SecondHand2iOS/SecondHand2iOS/DataLayer/API/SHAuthAPI.swift
@@ -17,9 +17,10 @@ class SHAuthAPI {
     func registerUserSecondHand(register: RegisterModel,
                             completionHandler: @escaping (Result<RegisterResponseModel, AFError>) -> Void) {
         let url = "https://market-final-project.herokuapp.com/auth/register"
-        let headers: HTTPHeaders = [.accept("body"), .contentType("multipart/form-data")]
-        AF.upload(multipartFormData: { (multipartFormData) in
-            <#code#> //code here
+        let headers: HTTPHeaders = [.accept("body"),
+                                    .contentType("multipart/form-data")]
+        AF.upload(multipartFormData: { multipartFormData in
+            //code here
         },
                   to: url,
                   method: .post,
@@ -55,21 +56,21 @@ class SHAuthAPI {
         }
     }
         
-        func updateUserSecondHand(access_token: String,
-                                  register: UpdateModel,
-                                  completionHandler: @escaping (Result<UpdateResponseModel, AFError>) -> Void) {
-            let url = "https://market-final-project.herokuapp.com/auth/user"
-            let headers: HTTPHeaders = ["accept": "body",
-                                        "access_token": "\(access_token)",
-                                        "Content-Type": "multipart/form-data"]
-            AF.upload(multipartFormData: { (multipartFormData) in
-                <#code#> //code here
-            },
-                      to: url,
-                      method: .put,
-                      headers: headers).response { response in
-                //code here
-            }
+    func updateUserSecondHand(access_token: String,
+                              register: UpdateModel,
+                              completionHandler: @escaping (Result<UpdateResponseModel, AFError>) -> Void) {
+        let url = "https://market-final-project.herokuapp.com/auth/user"
+        let headers: HTTPHeaders = ["accept": "body",
+                                    "access_token": "\(access_token)",
+                                    "Content-Type": "multipart/form-data"]
+        AF.upload(multipartFormData: { multipartFormData in
+            //code here
+        },
+                  to: url,
+                  method: .put,
+                  headers: headers).response { response in
+            //code here
+        }
     }
 }
 
