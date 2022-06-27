1786eaa2129dc56e1fb3695ac116a4ca1caeabb6
Muh Akbar Idris <muhakbaridris@gmail.com>
2022-06-27 20:44:22 +0800

    Add priceView and some fix

--- SecondHand2iOS/SecondHand2iOS/PresentationLayer/Buyer/Half Presentation/OverlayBuyerView.swift
+++ SecondHand2iOS/SecondHand2iOS/PresentationLayer/Buyer/Half Presentation/OverlayBuyerView.swift
@@ -12,19 +12,21 @@ final class OverlayBuyerView: UIViewController {
     @IBOutlet weak var btnSend: UIButton!
     @IBOutlet weak var sliderView: UIView!
     @IBOutlet weak var borderView: UIView!
+    @IBOutlet weak var priceView: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         btnSend.clipsToBounds = true
-        btnSend.layer.cornerRadius = 16
+        btnSend.layer.cornerRadius = 5
         sliderView.clipsToBounds = true
-        sliderView.layer.cornerRadius = 4
+        sliderView.layer.cornerRadius = 5
         borderView.clipsToBounds = true
         borderView.layer.backgroundColor = UIColor.white.cgColor
-        borderView.layer.cornerRadius = 16
+        borderView.layer.cornerRadius = 5
         borderView.layer.masksToBounds = false
         borderView.layer.shadowColor = UIColor.black.cgColor
         borderView.layer.shadowOffset = CGSize.zero
         borderView.layer.shadowRadius = 4
         borderView.layer.shadowOpacity = 0.15
+        priceView.layer.cornerRadius = 5
     }
 }
