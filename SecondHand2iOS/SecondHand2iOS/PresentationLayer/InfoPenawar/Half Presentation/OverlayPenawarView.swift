ca78299059b29e63b98fb61987eb98e2055f5e9f
Aiedyl <aiedyldava.contact@gmail.com>
2022-06-27 20:27:34 +0700

    radio button

--- SecondHand2iOS/SecondHand2iOS/PresentationLayer/InfoPenawar/Half Presentation/OverlayPenawarView.swift
+++ SecondHand2iOS/SecondHand2iOS/PresentationLayer/InfoPenawar/Half Presentation/OverlayPenawarView.swift
@@ -11,4 +11,49 @@ final class OverlayPenawarView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    var flag = false
+    var flag1 = false
+    
+    @IBOutlet var kirim: UIButton!
+    @IBOutlet var berhasil: UIButton!
+    @IBOutlet var batalkan: UIButton!
+    
+    @IBAction func berhasil(_ sender: UIButton) {
+        if(sender.isSelected){
+            sender.isSelected = false
+            batalkan.isSelected = false
+            kirim.isSelected = false
+        } else {
+            sender.isSelected = true
+            batalkan.isSelected = false
+            kirim.isSelected = true
+            if(kirim.isSelected){
+                kirim.backgroundColor =  UIColor(red: 104.0/255.0, green: 43.0/255.0, blue: 174.0/255.0, alpha: 1.0)
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func batalkan(_ sender: UIButton) {
+        if(sender.isSelected){
+            sender.isSelected = false
+            berhasil.isSelected = false
+            kirim.isSelected = false
+        } else {
+            sender.isSelected = true
+            berhasil.isSelected = false
+            kirim.isSelected = true
+            if(kirim.isSelected){
+                kirim.backgroundColor =  UIColor(red: 104.0/255.0, green: 43.0/255.0, blue: 174.0/255.0, alpha: 1.0)
+            }
+            
+        }
+        
+        
+    }
+    @IBAction func kirim(_ sender: Any) {
+        
+    }
 }
