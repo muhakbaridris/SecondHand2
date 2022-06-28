bf2aa98a2883db884383f7d27beec206059947f8
Maulana Frasha <ul.frasha@gmail.com>
2022-06-28 09:57:36 +0700

    add PanGestureSlide

--- SecondHand2iOS/SecondHand2iOS/PresentationLayer/InfoPenawar/OverlayPenawar/OverlayPenawarView.swift
+++ SecondHand2iOS/SecondHand2iOS/PresentationLayer/InfoPenawar/OverlayPenawar/OverlayPenawarView.swift
@@ -9,21 +9,54 @@ import UIKit
 
 final class OverlayPenawarView: UIViewController {
     
+    @IBOutlet weak var topSliderOutlet: UIView!
+    @IBOutlet weak var kirim: UIButton!
+    @IBOutlet weak var berhasil: UIButton!
+    @IBOutlet weak var batalkan: UIButton!
+    var flag = false
+    var flag1 = false
+    var hasSetPointOrigin = false
+    var pointOrigin: CGPoint?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         topSliderOutlet.clipsToBounds = true
         topSliderOutlet.layer.cornerRadius = 5
         kirim.layer.cornerRadius = 16
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerAction))
+        view.addGestureRecognizer(panGesture)
         
     }
     
-    var flag = false
-    var flag1 = false
+    override func viewDidLayoutSubviews() {
+        if !hasSetPointOrigin {
+            hasSetPointOrigin = true
+            pointOrigin = self.view.frame.origin
+        }
+    }
     
-    @IBOutlet weak var topSliderOutlet: UIView!
-    @IBOutlet var kirim: UIButton!
-    @IBOutlet var berhasil: UIButton!
-    @IBOutlet var batalkan: UIButton!
+    @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        // Not allowing the user to drag the view upward
+        guard translation.y >= 0 else { return }
+        
+        // setting x as 0 because we don't want users to move the frame side ways!! Only want straight up or down
+        view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + translation.y)
+        
+        if sender.state == .ended {
+            let dragVelocity = sender.velocity(in: view)
+            if dragVelocity.y >= 1300 {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                // Set back to original position of the view controller
+                UIView.animate(withDuration: 0.3) {
+                    self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: 400)
+                }
+            }
+        }
+    }
     
     @IBAction func berhasil(_ sender: UIButton) {
         if(sender.isSelected){
@@ -38,8 +71,6 @@ final class OverlayPenawarView: UIViewController {
                 kirim.backgroundColor =  UIColor(red: 104.0/255.0, green: 43.0/255.0, blue: 174.0/255.0, alpha: 1.0)
             }
         }
-        
-        
     }
     
     @IBAction func batalkan(_ sender: UIButton) {
@@ -54,11 +85,9 @@ final class OverlayPenawarView: UIViewController {
             if(kirim.isSelected){
                 kirim.backgroundColor =  UIColor(red: 104.0/255.0, green: 43.0/255.0, blue: 174.0/255.0, alpha: 1.0)
             }
-            
         }
-        
-        
     }
+    
     @IBAction func kirim(_ sender: Any) {
         
     }
