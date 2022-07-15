7cff5788c96a923fa2bbced2fbf557f25bc05369
nansyiiah <bosspetrik1403@gmail.com>
2022-07-15 19:35:24 +0700

    add daftar jual diminati

---
+++ SecondHand2iOS/SecondHand2iOS/DataLayer/API/Seller/SHSellerOrderAPI.swift
@@ -0,0 +1,21 @@
+//
+//  SHSellerOrderAPI.swift
+//  SecondHand2iOS
+//
+//  Created by Adrian K on 15/07/22.
+//
+
+import Foundation
+import Alamofire
+
+final class SHSellerOrderAPI{
+    
+    func getAllSellerOrder(access_token: String, status: String, completionHandler: @escaping (Result<[SHSellerOrderResponseModel], AFError>) -> Void) {
+        let url = "https://market-final-project.herokuapp.com/seller/order?status=\(status)"
+        let headers: HTTPHeaders = ["accept": "body",
+                                    "access_token": "\(access_token)"]
+        AF.request(url, method: .get, headers: headers).responseDecodable(of: [SHSellerOrderResponseModel].self){
+            response in completionHandler(response.result)
+        }
+    }
+}
---
+++ SecondHand2iOS/SecondHand2iOS/DataLayer/Model/SellerModel/SellerOrderModel/SHSellerOrderResponseModel.swift
@@ -0,0 +1,35 @@
+//
+//  SHSellerOrderAllResponseModel.swift
+//  SecondHand2iOS
+//
+//  Created by Adrian K on 15/07/22.
+//
+
+import Foundation
+
+struct SHSellerOrderResponseModel: Decodable {
+    let id, product_id, buyer_id, price: Int?
+    let transaction_date, product_name, base_price: String?
+    let image_product: String?
+    let status: String?
+    let Product: Product?
+    let User: User?
+}
+
+// MARK: - Product
+struct Product: Decodable {
+    let name, product_description: String?
+    let base_price: Int?
+    let image_url: String?
+    let image_name, location: String?
+    let user_id: Int?
+    let status: String?
+    let user: User?
+}
+
+// MARK: - User
+struct User: Decodable {
+    let id: Int?
+    let full_name, email, phone_number, address: String?
+    let city: String?
+}
